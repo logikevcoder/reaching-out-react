bd21e9f1fa30ffdfeccd9c44fabef910cd0e530b
“Kevin <logikevcoder@gmail.com>
2018-11-17 13:28:54 -0600

    fixed typo

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,7 +14,7 @@ class Blog extends Component {
 
     // using this lifecycle method as it's a passing function used for axios
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/post') // get the data from this api
+        axios.get('https://jsonplaceholder.typicode.com/posts') // get the data from this api
         .then(response => {
             const posts = response.data.slice(0, 4); // only store the first 4 returned items
             const updatedPosts = posts.map(post => {
